a752854de306469c6107816fd9f07635d6c54ebb
Shyam Shankar <shyam_rockinglife@hotmail.com>
2012-03-09 01:58:07 +0530

    Added test and other changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,27 +14,24 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class ApplicationController < ActionController::Base
-  before_filter :authorize
-  protect_from_forgery
+class SessionsController < ApplicationController
+  skip_before_filter :authorize
 
-  private
+  def new
+  end
 
-    def current_cart 
-      Cart.find(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound
-      cart = Cart.create
-      session[:cart_id] = cart.id
-      cart
+  def create
+    if user = User.authenticate(params[:name], params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, :alert => "Invalid user/password combination"
     end
+  end
 
-    # ...
+  def destroy
+    session[:user_id] = nil
+    redirect_to store_url, :notice => "Logged out"
+  end
 
-  protected
-
-    def authorize
-      unless User.find_by_id(session[:user_id])
-        redirect_to login_url, :notice => "Please log in"
-      end
-    end
 end
